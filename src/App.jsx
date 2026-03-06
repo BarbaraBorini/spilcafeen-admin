93abb718e33ca7149cabfb9cdfa1427883b50034
BarbaraBorini <eaa25babo@students.eaaa.dk>
2026-03-06 11:19:54 +0100

    vite config

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ const router = createBrowserRouter([
       { path: "create", element: <AddGamePage /> },
       { path: "update/:id", element: <EditGamePage /> },
     ],
+    basename: "/spilcaeen-admin",
   },
 ]);
 
